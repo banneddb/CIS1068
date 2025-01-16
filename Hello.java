bf4395dd3888bf1f4b17aa1076f019bbbaefc0f3
Behruz Omon <behruzomon6@gmail.com>
2025-01-16 10:07:15 -0500

    commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Welcome to cis 1068!");
+        System.out.println("Good morning, Professor Fiore.\n\nBehruz Omonullaev \n01/16/25 \nAssignment 1: Greetings\nIntroduce myself to my professor for Java this semester!");
     }
 }
