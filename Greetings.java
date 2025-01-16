64628376d39a34c44babffac7412e9d6596a0f3f
Behruz Omon <behruzomon6@gmail.com>
2025-01-16 10:09:12 -0500

    commmit

--- Greetings.java
+++ Greetings.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class Greetings {
     public static void main(String[] args) {
         System.out.println("Good morning, Professor Fiore.\n\nBehruz Omonullaev \n01/16/25 \nAssignment 1: Greetings\nIntroduce myself to my professor for Java this semester!");
     }
